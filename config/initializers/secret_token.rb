36ef45aa68a4bb5615845052bf4f36d5a62f1c95
Your Name <phillipkregg@gmail.com>
2012-10-28 19:55:43 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fit::Application.config.secret_token = 'a85e083a3bbdeb1fa2a18fd1a573ed0c5ce555d6de903e0bf18a945f11f53e1912e87f13c6790b655e244aa345c1cab36d16b76b980a271f9e6b18380229b34e'
